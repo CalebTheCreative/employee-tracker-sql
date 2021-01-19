65d26f3e9cce809101ccce733c2424a79f4c1055
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-19 15:28:06 -0600

    Added employees.js and dependencies.

---
+++ employees.js
@@ -0,0 +1,5 @@
+// Dependencies
+// =============================================================
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");

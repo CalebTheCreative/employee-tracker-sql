1c016878bc0b4154555204cbc48770a4eed488bd
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-19 14:03:48 -0600

    Added 'department' table.

--- schema.sql
+++ schema.sql
@@ -3,3 +3,9 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);

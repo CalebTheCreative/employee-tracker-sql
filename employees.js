326eb93ec1382c52a8f09e96a791412483beefa1
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-19 16:01:25 -0600

    Added main menu question.

--- employees.js
+++ employees.js
@@ -8,14 +8,38 @@ const consoleTable = require("console.table");
 // =============================================================
 const connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "",
     database: "employee_db"
-  });
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    mainMenu();
+});
+
+function mainMenu() {
+    inquirer.prompt({
+      name: 'mainOptions',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+          'Add Employee',
+          'Search For Employee',
+          'Update Employee Role',
+          'Add Role',
+          'Add Department',
+          'View All Employees',
+          'View All Roles',
+          'View All Departments',
+          'Quit'
+      ]
+    })
+}

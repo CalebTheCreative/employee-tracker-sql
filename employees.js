b9a92096a2173bf5544e1cc0b166e0d1b8c86961
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-19 16:14:58 -0600

    Added structure for main menu options.

--- employees.js
+++ employees.js
@@ -41,5 +41,73 @@ function mainMenu() {
           'View All Departments',
           'Quit'
       ]
-    })
+    }).then(function(answer) {
+        switch (answer.mainOptions) {
+            case 'Add Employee':
+                addEmployee();
+                break;
+
+            case 'Search For Employee':
+                searchEmployee();
+                break;
+
+            case 'Update Employee Role':
+                updateRole();
+                break;
+
+            case 'Add Role':
+                addRole();
+                break;
+
+            case 'Add Department':
+                addDept();
+                break;
+
+            case 'View All Employees':
+                viewEmployees();
+                break;
+
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'View All Departments':
+                viewDepts();
+                break;
+            case 'Quit':
+                finish();
+        }
+    });
 }
+
+function addEmployee() {
+
+}
+
+function searchEmployee() {
+
+}
+
+function updateRole() {
+
+}
+
+function addRole() {
+
+}
+
+function addDept() {
+
+}
+
+function viewEmployees() {
+
+}
+
+function viewRoles() {
+    
+}
+
+function viewDepts() {
+
+}
+

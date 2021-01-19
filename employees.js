6c0e724ccbdecc82eab6cdd598580adf206b640b
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-19 15:39:55 -0600

    Added MySQL server initiation.

--- employees.js
+++ employees.js
@@ -3,3 +3,19 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+
+// MySQL Server Initiation
+// =============================================================
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "employee_db"
+  });
